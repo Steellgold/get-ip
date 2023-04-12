4e0d2644724e9e48713363f02545277c395999cb
Gaëtan H <gaetanhus@gmail.com>
2023-04-12 19:47:54 +0200

    #ffffff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
   {#if location}
     <div class="flex items-center justify-center gap-3">
       <!-- Icon from tabler-icons.co -->
-      <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" stroke-width="0" fill="currentColor"></path>
       </svg>
@@ -37,7 +37,7 @@
 
     <div class="flex items-center justify-center gap-3">
       <!-- Icon from tabler-icons.co -->
-      <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <path d="M20 4v8"></path>
         <path d="M16 4.5v7"></path>
@@ -55,7 +55,7 @@
 
   <div class="flex items-center justify-center gap-3">
     <!-- Icon from tabler-icons.co -->
-    <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M6 10.5l1.436 -4c.318 -.876 .728 -1.302 1.359 -1.302c.219 0 1.054 .365 1.88 .583c.825 .219 .733 -.329 .908 -.487c.176 -.158 .355 -.294 .61 -.294c.242 0 .553 .048 1.692 .448c.759 .267 1.493 .574 2.204 .922c1.175 .582 1.426 .913 1.595 1.507l.816 4.623c2.086 .898 3.5 2.357 3.5 3.682c0 1.685 -1.2 3.818 -5.957 3.818c-6.206 0 -14.043 -4.042 -14.043 -7.32c0 -1.044 1.333 -1.77 4 -2.18z"></path>
       <path d="M6 10.5c0 .969 4.39 3.5 9.5 3.5c1.314 0 3 .063 3 -1.5"></path>
