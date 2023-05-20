e12cadea96378ac6035df7aaa8ecae12701735b0
Gaëtan H <steellgold0@gmail.com>
2023-05-20 23:08:17 +0200

    style(+page.svelte): change icon color to white

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
       <!-- Icon from tabler-icons.co -->
       <svg xmlns="http://www.w3.org/2000/svg" class="mt-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" stroke-width="0" fill="currentColor"></path>
+        <path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" stroke-width="0" fill="#ffffff"></path>
       </svg>
 
       <h3 class="text-xl sm:text-2xl font-bold text-white pt-2 hover:cursor-pointer" on:click={copyLocation}>
